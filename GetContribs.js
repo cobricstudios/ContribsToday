e871303f371b0126f3c3890f23d706a49baee79a
Kris <krissemicolon@users.noreply.github.com>
2020-06-08 12:35:37 +0200

    +Parsing SVG from GitHub to get daily commits.

--- GetContribs.js
+++ GetContribs.js
@@ -1,10 +1,16 @@
+const user = 'fabpot';
+const day = "2016-12-25";
 
-query {
-    user(login:"krissemicolon") {
-        contributionsCollection(from:"2020-06-01T00:00:00Z", to:"2020-06-01T00:00:00Z") {
-            contributionCalendar{
-                totalContributions
-            }
-        }
-    }
-}
+fetch('https://urlreq.appspot.com/req?method=GET&url=https%3A%2F%2Fgithub.com%2Fusers%2F' + user + '%2Fcontributions%3Fto%3D' + day)
+    .then(function(response) {
+        return response.text();
+    })
+    .then(function(text) {
+        xmlDoc = new DOMParser().parseFromString(text, 'text/xml');
+        var nodes = xmlDoc.getElementsByTagName('rect');
+        var dayContributions = nodes[nodes.length-1].getAttribute('data-count');
+        console.log('contributions count for ' + day + ' : ' + dayContributions);
+    })
+    .catch(function(error) {
+        console.log('Request failed', error)
+    });
