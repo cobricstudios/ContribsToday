540a221d23e037ffa9ebfa7d9dc8ed3461fce005
Kris <krissemicolon@users.noreply.github.com>
2020-06-14 18:22:50 +0200

    Contrib getter bugfix

--- GetContribs.js
+++ GetContribs.js
@@ -1,7 +1,7 @@
 const user = 'krissemicolon';
 const date = new Date();
 const daydate = date.getDate();
-const monthdate = date.getMonth();
+const monthdate = date.getMonth()+1;
 const yeardate = date.getFullYear();
 const day = yeardate+"-"+monthdate+"-"+daydate;
 
@@ -11,13 +11,12 @@ fetch('https://urlreq.appspot.com/req?method=GET&url=https%3A%2F%2Fgithub.com%2F
     })
     .then(function(text) {
         xmlDoc = new DOMParser().parseFromString(text, 'text/xml');
-        var nodes = xmlDoc.getElementsByTagName('rect');
-        var dayContributions = nodes[nodes.length-1].getAttribute('data-count');
-        console.log(dayContributions);
+        const nodes = xmlDoc.getElementsByTagName('rect');
+        const dayContributions = nodes[nodes.length - 1].getAttribute('data-count');
+        console.log("User: "+user+" Date: "+day+" Contribs: "+dayContributions);
     })
     .catch(function(error) {
         console.log('Request failed', error)
-        
     });
 
 chrome.browserAction.setBadgeText("5");
