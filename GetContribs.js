9ae2da1222d67ccb85784e30534054ca4992763e
Kris Huber <krissemicolon@gmail.com>
2020-08-20 11:52:45 +0200

    +Icon Numbers

--- GetContribs.js
+++ GetContribs.js
@@ -24,14 +24,16 @@ fetch('https://urlreq.appspot.com/req?method=GET&url=https%3A%2F%2Fgithub.com%2F
 
 let i = 1;                          //  set your counter to 1
 
-function myLoop() {                 //  create a loop function
+function updateContribs() {         //  create a loop function
     setTimeout(function() {  //  call a 3s setTimeout when the loop is called
         console.log('hello');       //  your code here
         i++;                        //  increment the counter
         if (i < 10) {               //  if the counter < 10, call the loop function
-            myLoop();               //  ..  again which will trigger another
+            updateContribs();        //  ..  again which will trigger another
         }                           //  ..  setTimeout()
     }, 3000)
 }
 
-myLoop();                           //  start the loop
+updateContribs();                           //  start the loop
+
+chrome.browserAction.setBadgeText({text: "2"});
