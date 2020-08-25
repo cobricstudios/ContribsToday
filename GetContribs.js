159e48abc61c49155277b6679a27443925caf2fe
Kris Huber <krissemicolon@gmail.com>
2020-08-25 14:02:05 +0200

    cool

--- GetContribs.js
+++ GetContribs.js
@@ -4,7 +4,9 @@ const daydate = date.getDate();
 const monthdate = date.getMonth()+1;
 const yeardate = date.getFullYear();
 const day = yeardate+"-"+monthdate+"-"+daydate;
+let dayContributions
 
+function Fetch() {
 fetch('https://urlreq.appspot.com/req?method=GET&url=https%3A%2F%2Fgithub.com%2Fusers%2F' + user + '%2Fcontributions%3Fto%3D' + day)
     .then(function(response) {
         return response.text();
@@ -12,28 +14,30 @@ fetch('https://urlreq.appspot.com/req?method=GET&url=https%3A%2F%2Fgithub.com%2F
     .then(function(text) {
         xmlDoc = new DOMParser().parseFromString(text, 'text/xml');
         const nodes = xmlDoc.getElementsByTagName('rect');
-        const dayContributions = nodes[nodes.length - 1].getAttribute('data-count');
+        dayContributions = nodes[nodes.length - 1].getAttribute('data-count');
         console.log("User: "+user+" Date: "+day+" Contribs: "+dayContributions);
+
     })
     .catch(function(error) {
         console.log('Request failed', error)
     });
 
+}
 
 
+let i = 1
 
-let i = 1;                          //  set your counter to 1
-
-function updateContribs() {         //  create a loop function
-    setTimeout(function() {  //  call a 3s setTimeout when the loop is called
-        console.log('hello');       //  your code here
-        i++;                        //  increment the counter
-        if (i < 10) {               //  if the counter < 10, call the loop function
-            updateContribs();        //  ..  again which will trigger another
-        }                           //  ..  setTimeout()
+function updateContribs() {
+    setTimeout(function() {
+        console.log("User: "+user+" Date: "+day+" Contribs: "+dayContributions);
+        i++;
+        if (i < 10) {
+            Fetch();
+            updateContribs();
+        }
     }, 3000)
 }
 
 updateContribs();                           //  start the loop
 
-chrome.browserAction.setBadgeText({text: "2"});
+chrome.browserAction.setBadgeText({text: ""});
